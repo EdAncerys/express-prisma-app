9dd96511b9a2e1856f07a2d96f39733a45637554
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-10-29 15:59:41 +0000

    Add types

---
+++ types/Joke.ts
@@ -0,0 +1,19 @@
+// types/Joke.ts
+export interface Joke {
+  error: boolean;
+  category: string;
+  type: 'single' | 'twopart';
+  setup?: string;
+  delivery?: string;
+  flags: {
+    nsfw: boolean;
+    religious: boolean;
+    political: boolean;
+    racist: boolean;
+    sexist: boolean;
+    explicit: boolean;
+  };
+  id: number;
+  safe: boolean;
+  lang: string;
+}
